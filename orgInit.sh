7ef06b108980df4b87f2e79f01756ed196bc7764
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-15 15:18:24 -0700

    New release

--- orgInit.sh
+++ orgInit.sh
@@ -1,9 +1,9 @@
-#sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o mobile.demo
-sfdx force:org:create -f config/project-scratch-def.json -a salesforce-mobile -s -d 30 -w 60
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o mobile.demo
+#sfdx force:org:create -f config/project-scratch-def.json -a salesforce-mobile -s -d 30 -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n SalesforceMobile
 sfdx shane:user:password:set -g User -l User -p salesforce1
-#sfdx automig:dump --objects Task --outputdir ./data
+#sfdx automig:dump --objects Account,Contact,Vehicle__c,Office__c,Meter_Reading__c --outputdir ./data
 sfdx force:org:open
 sfdx automig:load --inputdir ./data
-sfdx force:apex:execute --apexcodefile ./scripts/generateMeterReadings.apex
+#sfdx force:apex:execute --apexcodefile ./scripts/generateMeterReadings.apex
